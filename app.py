47c583e91ee4a4228a9d8fc1572ed2f384ca859b
Tucker <tmiles7@vols.utk.edu>
2020-08-13 23:41:55 -0400

    Function handle_data added

--- app.py
+++ app.py
@@ -43,32 +43,43 @@ def calculateTime(result):
     print('Distance: {}\nPace: {}\n'.format(distance, pace))
     print('Time is {} seconds'.format(distance * pace))
 
+# https://stackoverflow.com/questions/11556958/sending-data-from-html-form-to-a-python-script-in-flask
+# using this instead of the jinja forms stuff
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    print("here")
+    data = request.form.get('distance')
+
+    return render_template('index.html')
 
 # Base url
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = ComputeForm()
-    if form.is_submitted():
-        result = request.form # import request
 
-        if result['hours'] != '' and result['minutes'] != '' and result['seconds'] != '':
-            givenTime = True
-        else:
-            givenTime = False
+    return render_template('index.html')
+
+    # form = ComputeForm()
+    # if form.is_submitted():
+    #     result = request.form # import request
+
+    #     if result['hours'] != '' and result['minutes'] != '' and result['seconds'] != '':
+    #         givenTime = True
+    #     else:
+    #         givenTime = False
 
 
-        if result['distance'] != '' and givenTime and result['pace'] == '':
-            calculatePace(result)
-        elif result['pace'] != '' and givenTime and result['distance'] == '':
-            calculateDistance(result)
-        elif result['distance'] != '' and result['pace'] != '' and not givenTime:
-            calculateTime(result)
-        else:
-            # error and die
-            # raise ValueError('2 Fields must be completed and 1 must be blank!')
-            print('2 Fields must be completed and 1 must be blank!')
+    #     if result['distance'] != '' and givenTime and result['pace'] == '':
+    #         calculatePace(result)
+    #     elif result['pace'] != '' and givenTime and result['distance'] == '':
+    #         calculateDistance(result)
+    #     elif result['distance'] != '' and result['pace'] != '' and not givenTime:
+    #         calculateTime(result)
+    #     else:
+    #         # error and die
+    #         # raise ValueError('2 Fields must be completed and 1 must be blank!')
+    #         print('2 Fields must be completed and 1 must be blank!')
 
-    return render_template('index.html', form=form)
+    # return render_template('index.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
