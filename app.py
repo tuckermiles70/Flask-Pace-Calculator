9eb626173f2a491419dca708245352c7b3b60060
Tucker <tmiles7@vols.utk.edu>
2020-08-14 00:09:57 -0400

    Barebones application working

--- app.py
+++ app.py
@@ -54,17 +54,33 @@ def handle_data():
     # print("here")
     result = request.form
 
+    if result['hours'] == '' and result['minutes'] == '' and result['seconds'] == '':
+        # error
+        pass
+    
+    if result['distance'] == '':
+        # error
+        pass
+
     totalseconds = 0
-    totalseconds += int(result['hours']) * 3600
-    totalseconds += int(result['minutes']) * 60
-    totalseconds += int(result['seconds'])
+    
+    if result['hours'] != '':
+        totalseconds += int(result['hours']) * 3600
+    if result['minutes'] != '':
+        totalseconds += int(result['minutes']) * 60
+    if result['seconds'] != '':
+        totalseconds += int(result['seconds'])
 
     distance = float(result['distance'])
 
     print('Distance: {}\nTime: {}\n'.format(distance, totalseconds))
     print('Pace is {} seconds per mile'.format(totalseconds / distance))
 
-    return render_template('index.html', pace = totalseconds / distance)
+    pace_in_seconds = totalseconds / distance
+
+    pace_in_minutes = pace_in_seconds / 60
+
+    return render_template('index.html', pace = pace_in_minutes)
 
     # if result['hours'] != '' and result['minutes'] != '' and result['seconds'] != '':
     #     givenTime = True
